1790562a831be284725172d94c47532963e0a2ca
ZokaKami <ruffy92@live.com>
2024-07-26 14:07:03 +0200

    Update gallery.tsx

--- src/components/gallery/gallery.tsx
+++ src/components/gallery/gallery.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { comissionsImages } from "./comissionsImages.js";
 export default function Gallery({ triggerRef }) {
   return (
-    <div ref={triggerRef} className="ml-[33vw]   ">
-      <aside className="h-[50vh]   text-center text-white flex place-content-center border-dotted 	border-b-[0.2vw] mx-[0.5vw] border-[#B692C2] ">
+    <div ref={triggerRef} className="pl-[33vw]   ">
+      <aside className="h-[50vh]   text-center text-[#694F8E] flex place-content-center border-dotted 	border-b-[0.2vw] mx-[0.5vw] border-[#B692C2] ">
         <h1 className="grid  place-content-center text-[6vw] "> XCOMISSIONS</h1>
       </aside>
       <div className="grid grid-cols-2 ">
@@ -17,7 +17,7 @@ export default function Gallery({ triggerRef }) {
             />
           </div>
         ))}
-        <div className="   my-auto text-right pr-[3vw] text-[4vw]">
+        <div className="text-[#694F8E]  my-auto text-right pr-[3vw] text-[4vw]">
           <button className="text-right">
             <p>FULL </p>
             <p>GALLERY</p>
